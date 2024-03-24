dad9e0e0df373a8069c2beb887c5ba50dc89f695
Furkan-Boncuk <furkanboncuk2000@gmail.com>
2024-03-25 00:54:36 +0300

    Takvimli planlama template'i için gerekli componentler oluşturulmaya başlandı. business-components içerisindeki büyük componentlerin küçük parçalarının ui-components üzerinden verilmesi planlanıyor.

---
+++ src/ui-components/ImportantTopics/CalendarTopic.tsx
@@ -0,0 +1,45 @@
+import { Flex, Text } from "@chakra-ui/react";
+import {
+  faCircleInfo,
+  faCircleXmark,
+  faPen,
+  faTag,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { rp } from "../../../public/colors/colors";
+import React from "react";
+
+export default function CalendarTopic() {
+  return (
+    <Flex
+      alignItems={"center"}
+      padding={"5px 10px"}
+      justifyContent={"space-between"}
+      gap={"8px"}
+      backgroundColor={rp.app.calendarTopicsBg}
+      borderRadius={"5px"}
+    >
+      <Flex alignItems={"center"} gap={"8px"}>
+        <FontAwesomeIcon icon={faTag} color="red" fontSize={"22px"} />
+        <Text fontSize={"17px"}>Vize Sınavları</Text>
+      </Flex>
+      <Flex gap={"8px"}>
+        <FontAwesomeIcon
+          icon={faCircleInfo}
+          cursor={"pointer"}
+          color={rp.app.icons.info}
+        />
+        <FontAwesomeIcon
+          icon={faCircleXmark}
+          cursor={"pointer"}
+          color={rp.app.icons.circleX}
+        />
+        <FontAwesomeIcon
+          icon={faPen}
+          cursor={"pointer"}
+          color={rp.app.icons.pen}
+        />
+      </Flex>
+    </Flex>
+  );
+}
---
+++ src/ui-components/ImportantTopics/CreateNewTopicModal.tsx
@@ -0,0 +1,31 @@
+import { Flex, Input, Text } from "@chakra-ui/react";
+import { faTag } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { rp } from "../../../public/colors/colors";
+import React from "react";
+
+export default function CreateNewTopicModal() {
+  return (
+    <Flex
+      position={"relative"}
+      backgroundColor={rp.app.calendarTopicsInputBg}
+      padding={"5px 10px"}
+      borderRadius={"5px"}
+    >
+      <Input
+        outline={"none"}
+        padding={"10px"}
+        border={"none"}
+        backgroundColor={"transparent"}
+        autoFocus={true}
+        width={"25vw"}
+        fontSize={"17px"}
+        placeholder="Vize Sınavları, Spor, Beslenme vb..."
+        pl={"40px"}
+      />
+      <Text position={"absolute"} bottom={"-8px"} left={"10px"}>
+        <FontAwesomeIcon icon={faTag} color="orange" fontSize={"30px"} />
+      </Text>
+    </Flex>
+  );
+}
